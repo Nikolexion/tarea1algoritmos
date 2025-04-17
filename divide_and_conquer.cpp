a5a7b295a7db40c69183e700f77814e70b6f9e3b
Nikolexion <nfalc2003@gmail.com>
2025-04-17 09:24:19 -0400

    feat: plantilla para divide and conquer

---
+++ divide_and_conquer.cpp
@@ -0,0 +1,5 @@
+#include "divide_and_conquer.h"
+
+double divide_and__conquer(std::pair<double,double> n[], int tam){
+    
+}
---
+++ divide_and_conquer.h
@@ -0,0 +1,7 @@
+#ifndef DIVIDE_AND_CONQUER
+#define DIVIDE_AND_CONQUER
+#include "distancia_euc.h"
+
+double divide_and_conquer(std::pair<double,double> n[], int tam);
+
+#endif

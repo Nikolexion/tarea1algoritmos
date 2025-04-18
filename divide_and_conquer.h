87a701af184149c0c71011c8c11a16cbced73cab
Nikolexion <nfalc2003@gmail.com>
2025-04-18 13:57:53 -0400

    fix: ahora divide and conquer funciona como en geeksforgeeks

--- divide_and_conquer.h
+++ divide_and_conquer.h
@@ -2,6 +2,6 @@
 #define DIVIDE_AND_CONQUER
 #include "distancia_euc.h"
 
-double divide_and_conquer(std::pair<double,double> n[], int tam);
+double divide_and__conquer(std::pair<double, double> n[], int tam, bool esta_desordenado);
 
 #endif

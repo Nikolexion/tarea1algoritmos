8971778823b5508fcdf1a504dfb79bc02c04c1cf
Nikolexion <nfalc2003@gmail.com>
2025-04-19 12:11:56 -0400

    fix: corregido el = en vez de -

--- distancia_euc.cpp
+++ distancia_euc.cpp
@@ -1,5 +1,5 @@
 #include "distancia_euc.h"
 
 double calculateDistance(std::pair<double,double> p1, std::pair<double, double> p2) {
-    return sqrt(pow(p1.first = p2.first, 2) + pow(p1.second= p2.second, 2));
+    return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
     }
